0fc9c5f4c9ff9b1632704cc0eca541e23c9b40a2
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-11-09 00:02:06 +0530

    Checking if user already exists in DB

--- routes/auth.js
+++ routes/auth.js
@@ -12,7 +12,14 @@ router.post('/register', async (req,res) => {
     {
         return res.status(400).send(error.details[0].message);
     }
+    //checking if user already exists in the database
+    const emailExist = await User.findOne({email:req.body.email});
+    if(emailExist)
+    {
+        return res.status(400).send('Email already exists');
+    }
 
+    //Create a new user
     const user = new User({
         name:req.body.name,
         email:req.body.email,
